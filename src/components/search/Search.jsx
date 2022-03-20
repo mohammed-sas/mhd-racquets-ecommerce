0e9a1fc9f19aabb4d1bdca7800dd96e0c9bb6215
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-03-20 23:10:06 +0530

    feat : clicking on search results would take to corresponding product

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import classes from '../Navbar/navbar.module.css'
 const Search = ({list}) => {
+    const navigate = useNavigate();
     return (
         <div className={classes["search-list"]}>
             <ul>
                 {
                     list.map(item=>{
-                        return <li key={item.id}>
+                        return <li key={item.id} onClick={()=>navigate(`/product/${item._id}`)}>
                             <div>
                             <p>{item.title}</p>
                             <small>in <strong>{item.categoryName}</strong></small>
