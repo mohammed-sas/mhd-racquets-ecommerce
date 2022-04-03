c82c790a136c79595b1fbb7ac27c13af2a201beb
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-04-03 17:56:15 +0530

    styling : adjusted the home layout sub headers

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -28,71 +28,75 @@ const Home = () => {
             <button className="btn btn-primary" onClick={()=>navigate("/products-listing")}>Shop Now</button>
           </div>
         </div>
-        <h2 className="centered-text grey">Featured Categories</h2>
+       
         <div className={classes["featured-categories"]}>
-          <div className={classes["featured-item"]} onClick={()=>featuredCategoriesHandler("Racquet")}>
-            <img src={racquet3} alt="racquet" />
-            <h3>Racquets</h3>
-          </div>
-          <div className={classes["featured-item"]} onClick={()=>featuredCategoriesHandler("ShuttleCock")}>
-            <img src={shuttle} alt="shuttle" />
-            <h3>Shuttlecock</h3>
-          </div>
-          <div className={classes["featured-item"]} onClick={()=>featuredCategoriesHandler("String")}>
-            <img src={stringImage} alt="string" />
-            <h3>String</h3>
-          </div>
-          <div className={classes["featured-item"]} onClick={()=>featuredCategoriesHandler("Shoe")}>
-            <img src={shoeImage} alt="shoe" />
-            <h3>Shoe</h3>
+        <h2 className="centered-text grey">Featured Categories</h2>
+          <div className={classes["featured-lists"]}>
+            <div className={classes["featured-item"]} onClick={()=>featuredCategoriesHandler("Racquet")}>
+              <img src={racquet3} alt="racquet" />
+              <h3>Racquets</h3>
+            </div>
+            <div className={classes["featured-item"]} onClick={()=>featuredCategoriesHandler("ShuttleCock")}>
+              <img src={shuttle} alt="shuttle" />
+              <h3>Shuttlecock</h3>
+            </div>
+            <div className={classes["featured-item"]} onClick={()=>featuredCategoriesHandler("String")}>
+              <img src={stringImage} alt="string" />
+              <h3>String</h3>
+            </div>
+            <div className={classes["featured-item"]} onClick={()=>featuredCategoriesHandler("Shoe")}>
+              <img src={shoeImage} alt="shoe" />
+              <h3>Shoe</h3>
+            </div>
           </div>
         </div>
-        <h2 className="centered-text grey">New Arrivals</h2>
+        
         <div className={classes["new-arrival-container"]}>
-          <div className={`${classes["card-container"]}  ${classes["card-width-40"]} ${classes["bg-purple-50"]} ${classes["padding-1rem"]}`}>
-            <div className="horizontal-card">
-              <div className="card-image-basic">
-                <img src={racquet1} alt="gold medal edition" />
-              </div>
-              <div className="card-heading">
-                <div className={"padding-l-r-16-b-5 heading "+ classes["bg-purple-50"]}>
-                  Gold medal Limited Edition
+        <h2 className="centered-text grey">New Arrivals</h2>
+          <div className={classes["new-arrival-lists"]}>
+            <div className={`${classes["card-container"]}  ${classes["card-width-40"]} ${classes["bg-purple-50"]} ${classes["padding-1rem"]}`}>
+              <div className="horizontal-card">
+                <div className="card-image-basic">
+                  <img src={racquet1} alt="gold medal edition" />
                 </div>
-                <div className={"padding-l-r-16-b-5 sub-heading "+ classes["bg-purple-50"]}>
-                  by Yonex
-                </div>
-              </div>
-            </div>
-            <div className={`footer-links ${classes["fluid-x"]} ${classes["bg-purple-50"]} ${classes["padding-1rem"]}`}>
-              <button className="icon-btn">
-                <i className="fas fa-heart"></i>
-              </button>
-              <button className="btn btn-primary">BUY NOW</button>
-            </div>
-          </div>
-
-          <div className={`${classes["card-container"]}  ${classes["card-width-40"]} ${classes["bg-purple-50"]} ${classes["padding-1rem"]}`}>
-            <div className="horizontal-card">
-              <div className="card-image-basic">
-                <img src={racquet2} alt="aeronaut 4000" />
-              </div>
-              <div className="card-heading  ">
-                <div className={"padding-l-r-16-b-5 heading "+ classes["bg-purple-50"]}>
-                  Aeronaut 4000 boost
-                </div>
-                <div className={"padding-l-r-16-b-5 sub-heading "+ classes["bg-purple-50"]}>
-                  by Yonex
+                <div className="card-heading">
+                  <div className={"padding-l-r-16-b-5 heading "+ classes["bg-purple-50"]}>
+                    Gold medal Limited Edition
+                  </div>
+                  <div className={"padding-l-r-16-b-5 sub-heading "+ classes["bg-purple-50"]}>
+                    by Yonex
+                  </div>
                 </div>
               </div>
+              <div className={`footer-links ${classes["fluid-x"]} ${classes["bg-purple-50"]} ${classes["padding-1rem"]}`}>
+                <button className="icon-btn">
+                  <i className="fas fa-heart"></i>
+                </button>
+                <button className="btn btn-primary">BUY NOW</button>
+              </div>
             </div>
-            <div className={`footer-links ${classes["fluid-x"]} ${classes["bg-purple-50"]} ${classes["padding-1rem"]}`}>
-              <button className="icon-btn">
-                <i className="fas fa-heart"></i>
-              </button>
-              <button className="btn btn-primary">BUY NOW</button>
+            <div className={`${classes["card-container"]}  ${classes["card-width-40"]} ${classes["bg-purple-50"]} ${classes["padding-1rem"]}`}>
+              <div className="horizontal-card">
+                <div className="card-image-basic">
+                  <img src={racquet2} alt="aeronaut 4000" />
+                </div>
+                <div className="card-heading  ">
+                  <div className={"padding-l-r-16-b-5 heading "+ classes["bg-purple-50"]}>
+                    Aeronaut 4000 boost
+                  </div>
+                  <div className={"padding-l-r-16-b-5 sub-heading "+ classes["bg-purple-50"]}>
+                    by Yonex
+                  </div>
+                </div>
+              </div>
+              <div className={`footer-links ${classes["fluid-x"]} ${classes["bg-purple-50"]} ${classes["padding-1rem"]}`}>
+                <button className="icon-btn">
+                  <i className="fas fa-heart"></i>
+                </button>
+                <button className="btn btn-primary">BUY NOW</button>
+              </div>
             </div>
-          </div>
-        </div>
+          </div></div>
       </main>
   );
 };
