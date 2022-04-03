733cd9387570e8f497eebe03e1019b6db3de6106
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-04-03 23:29:49 +0530

    refactor:barrel exported pages

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import classes from './app.module.css'
-import Signup from "./pages/signup/Signup";
-import {Routes,Route} from 'react-router-dom';
-import Home from './pages/home/Home'
-import Login from './pages/login/Login';
-import Cart from "./pages/cart/Cart";
-import Products from "./pages/products/Products";
-import MockAPI from "./components/mockman/MockAPI";
-import Wishlist from "./pages/wishlist/Wishlist";
-import SingleProduct from "./pages/single product/SingleProduct";
-import {Navbar} from "./components";
-import Profile from './pages/profile/Profile';
-import OrderSummary from './pages/order summary/OrderSummary';
+import classes from "./app.module.css";
+import { Routes, Route } from "react-router-dom";
+import Mockman from "mockman-js";
+import { Navbar } from "./components";
+import {
+  Home,
+  Login,
+  Cart,
+  Products,
+  Wishlist,
+  SingleProduct,
+  OrderSummary,
+  Profile,
+  Signup,
+} from "./pages";
 function App() {
-  
   return (
     <div className={classes["app-container"]}>
-       <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/cart" element={<Cart/>}/>
-        <Route path="/products-listing" element={<Products/>}/>
-        <Route path="/mock-api" element={<MockAPI/>}/>
-        <Route path="/wishlist" element={<Wishlist/>}/>
-        <Route path="/product/:productId" element={<SingleProduct/>}/>
-        <Route path="/profile/address" element={<Profile/>} />
-        <Route path="/order-summary" element={<OrderSummary/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/products-listing" element={<Products />} />
+        <Route path="/mock-api" element={<Mockman />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/product/:productId" element={<SingleProduct />} />
+        <Route path="/profile/address" element={<Profile />} />
+        <Route path="/order-summary" element={<OrderSummary />} />
       </Routes>
-
     </div>
   );
 }
