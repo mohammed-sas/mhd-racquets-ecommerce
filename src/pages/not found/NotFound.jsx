a1d5686b8fdf766dad640974591de55147c24de2
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-04-07 12:47:27 +0530

    404 page

---
+++ src/pages/not found/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2 className="centered-text">Error: 404, the page you are searching for does not exist</h2>
+        </div>
+    )
+}
+
+export default NotFound
