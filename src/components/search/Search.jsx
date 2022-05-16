a0c5b47d2ade30f4915209ac1c6cd7a28ee6be72
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-05-16 17:33:56 +0530

    fix:hiding search result after clicking on list

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import classes from '../Navbar/navbar.module.css'
-const Search = ({list}) => {
-    const navigate = useNavigate();
-    return (
-        <div className={classes["search-list"]}>
-            <ul>
-                {
-                    list.map(item=>{
-                        return <li key={item.id} onClick={()=>navigate(`/product/${item._id}`)}>
-                            <div>
-                            <p>{item.title}</p>
-                            <small>in <strong>{item.categoryName}</strong></small>
-                            </div>
-                        </li>
-                    })
-                }
-            </ul>
-        </div>
-    )
-}
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import classes from "../Navbar/navbar.module.css";
+const Search = ({ list, setSearchResult }) => {
+  const navigate = useNavigate();
+  const clickHandler = (id) => {
+    navigate(`/product/${id}`);
+    setSearchResult([]);
+  };
+  return (
+    <div className={classes["search-list"]}>
+      <ul>
+        {list.map((item) => {
+          return (
+            <li key={item.id} onClick={()=>clickHandler(item._id)}>
+              <div>
+                <p>{item.title}</p>
+                <small>
+                  in <strong>{item.categoryName}</strong>
+                </small>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
